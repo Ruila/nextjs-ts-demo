54273a1931e1066bd08664d33804539493a1f8bd
Ruila <4momolmr@gmail.com>
2023-03-27 13:19:54 +0800

    feat: add example for swr

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import useSWR from 'swr'
 
-const inter = Inter({ subsets: ['latin'] })
+
+const fetcher = url => fetch(url).then(r => r.json());
+const inter = Inter({subsets: ['latin']})
 export default function Home() {
+
+    const {data, error} = useSWR(
+        "https://jsonplaceholder.typicode.com/todos/2",
+        fetcher,
+        {}
+    );
     return (<h2 className={`${inter.className} ${styles.fontRed} text-3xl font-bold underline`}>
-        Home
+        Home: {data?.title}
     </h2>)
 }
